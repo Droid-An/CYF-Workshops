2c6554e3da94b78431c93ed4236061f167f45afa
Droid-An <andrei.filippov.aaf@gmail.com>
2025-07-20 15:27:20 +0100

    objects workshop

--- objects/200.js
+++ objects/200.js
@@ -4,11 +4,11 @@
 // Fix anything that needs fixing.
 
 const person = {
-    "name": "Jemima",
-    "location",
-    "id_number" = 9,
+  name: "Jemima",
+  location: "",
+  id_number: 9,
 };
 
-console.assert(person.name === "Jemima");
-console.assert(person.location === "Glasgow");
-console.assert(person.id_number === 9);
+console.assert(person.name === "Jemima", "Name should be Jemima");
+console.assert(person.location === "Glasgow", "should be Glasgow");
+console.assert(person.id_number === 9, "should be 9");
